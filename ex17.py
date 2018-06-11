1259aaf51635d2e93a951f9cbe4bdef566c13089
khinphyothin <1khin9phyo9thin7@gmail.com>
2018-06-11 00:42:50 -0700

    exercises

---
+++ ex17.py
@@ -0,0 +1,24 @@
+form sys import argv
+form os.patch import exists
+
+script,form_file,to_file=argv
+
+print(f"Copying form {form_file}to{to_file}")
+
+#we could do these two on one line,how?
+in_file=open(from_file)
+indata=in_file.read()
+
+print(f"The input file is {len(indata)}byetes ling")
+
+print(f"Does the output file exist?{exists(to_file)}")
+print("Ready,hit RETURN to continue,CTRL_C to abort.")
+input()
+
+out_file=open(to_file,'w')
+out_file.write(indata)
+
+print("Alright,all done.")
+
+out_file.close()
+in_file.close()
